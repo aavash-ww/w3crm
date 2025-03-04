4821a40a94a558a333606d155991c290607a649a
thapaaavash750@gmail.com <thapaaavash750@gmail.com>
2025-03-04 07:55:14 +0545

    review

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -1,93 +1,102 @@
 <template>
-    <div class="review">
-        <div class="review__image">
-            <img class="review__image__person" src="../assets/images/review.jpeg" alt="">
-            <img class="review__image__feature" src="../assets/images/featuree.png" alt="">
+    <div style="background: #cceef9;">
+        <div class="review">
+            <div class="review__image">
+                <img class="review__image__person" src="../assets/images/review.jpeg" alt="">
+                <img class="review__image__feature" src="../assets/images/featuree.png" alt="">
 
-        </div>
-        <div class="review__text">
-            <div class="review__text__wrap">
-                <h3>Contribute on Sales and Management</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, minus? Lorem, ipsum dolor sit
-                    amet consectetur adipisicing elit. Provident, tempora?</p>
-                <div class="review__text__wrap__review">
-                    <img src="../assets/images/quotes.svg" alt="">
-
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptatibus aut labore
-                        molestiae
-                        ullam
-                        doloribus accusantium dicta ratione officia placeat.</p>
-                </div>
             </div>
-            <div class="review__text__name">
-                <span class="review__text__name__main">
-                    Nirmal Gaywali
-                </span> <br>
-                <span class="review__text__name__position">
-                    Manager, The Gurkha Kitchen
-                </span>
+            <div class="review__text">
+                <div class="review__text__wrap">
+                    <h3>Contribute on Sales and Management</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, minus? Lorem, ipsum dolor
+                        sit
+                        amet consectetur adipisicing elit. Provident, tempora?</p>
+                    <div class="review__text__wrap__review">
+                        <img src="../assets/images/quotes.svg" alt="">
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptatibus aut labore
+                            molestiae
+                            ullam
+                            doloribus accusantium dicta ratione officia placeat.</p>
+                    </div>
+                </div>
+                <div class="review__text__name">
+                    <span class="review__text__name__main">
+                        Nirmal Gaywali
+                    </span> <br>
+                    <span class="review__text__name__position">
+                        Manager, The Gurkha Kitchen
+                    </span>
+                </div>
             </div>
         </div>
     </div>
     <!-- anotehr  -->
-    <div class="review">
-        <div class="review__text" style="text-align: right;">
-            <div class="review__text__wrap" >
-                <h3>Save hours with automated lead nurturing</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, minus? Lorem, ipsum dolor sit
-                    amet consectetur adipisicing elit. Provident, tempora?</p>
-                <div class="review__text__wrap__review">
-                    <img src="../assets/images/quotes.svg" alt="">
+    <div style="background: #9edef4;">
+        <div class="review">
+            <div class="review__text">
+                <div class="review__text__wrap">
+                    <h3>Save hours with automated lead nurturing</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, minus? Lorem, ipsum dolor
+                        sit
+                        amet consectetur adipisicing elit. Provident, tempora?</p>
+                    <div class="review__text__wrap__review" >
+                        <img src="../assets/images/quotes.svg" alt="">
 
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptatibus aut labore
-                        molestiae
-                        ullam
-                        doloribus accusantium dicta ratione officia placeat.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptatibus aut labore
+                            molestiae
+                            ullam
+                            doloribus accusantium dicta ratione officia placeat.</p>
+                    </div>
+                </div>
+                <div class="review__text__name">
+                    <span class="review__text__name__main">
+                        Nirmal Gaywali
+                    </span> <br>
+                    <span class="review__text__name__position">
+                        Manager, The Gurkha Kitchen
+                    </span>
                 </div>
             </div>
-            <div class="review__text__name">
-                <span class="review__text__name__main">
-                    Nirmal Gaywali
-                </span> <br>
-                <span class="review__text__name__position">
-                    Manager, The Gurkha Kitchen
-                </span>
-            </div>
-        </div>
-        <div class="review__image">
-            <img class="review__image__person" src="../assets/images/avatarimg4.jpg" alt="">
-            <img class="review__image__feature" src="../assets/images/featuree.png" alt="">
+            <div class="review__image" style="padding-left: 1rem;">
+                <img class="review__image__person" src="../assets/images/avatarimg4.jpg" alt="">
+                <img class="review__image__feature" src="../assets/images/featuree.png" alt="">
 
+            </div>
         </div>
     </div>
     <!-- another  -->
-    <div class="review">
-        <div class="review__image">
-            <img class="review__image__person" src="../assets/images/partnercard2.jpeg" alt="">
-            <img class="review__image__feature" src="../assets/images/featuree.png" alt="">
+    <div style="background: #cceef9;">
+        <div class="review">
+            <div class="review__image">
+                <img class="review__image__person" src="../assets/images/partnercard2.jpeg" alt="">
+                <img class="review__image__feature" src="../assets/images/featuree.png" alt="">
 
-        </div>
-        <div class="review__text">
-            <div class="review__text__wrap">
-                <h3>Overcome Issues with our CRM solutions</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, minus? Lorem, ipsum dolor sit
-                    amet consectetur adipisicing elit. Provident, tempora?</p>
-                <div class="review__text__wrap__review">
-                    <img src="../assets/images/quotes.svg" alt="">
-
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptatibus aut labore
-                        molestiae
-                        ullam
-                        doloribus accusantium dicta ratione officia placeat.</p>
-                </div>
             </div>
-            <div class="review__text__name">
-                <span class="review__text__name__main">
-                    Nirmal Gaywali
-                </span> <br>
-                <span class="review__text__name__position">
-                    Manager, The Gurkha Kitchen
-                </span>
+            <div class="review__text">
+                <div class="review__text__wrap">
+                    <h3>Overcome Issues with our CRM solutions</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, minus? Lorem, ipsum dolor
+                        sit
+                        amet consectetur adipisicing elit. Provident, tempora?</p>
+                    <div class="review__text__wrap__review">
+                        <img src="../assets/images/quotes.svg" alt="">
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptatibus aut labore
+                            molestiae
+                            ullam
+                            doloribus accusantium dicta ratione officia placeat.</p>
+                    </div>
+                </div>
+                <div class="review__text__name">
+                    <span class="review__text__name__main">
+                        Nirmal Gaywali
+                    </span> <br>
+                    <span class="review__text__name__position">
+                        Manager, The Gurkha Kitchen
+                    </span>
+                </div>
             </div>
         </div>
     </div>
